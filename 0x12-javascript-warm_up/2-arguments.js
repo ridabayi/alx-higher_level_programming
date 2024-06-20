490a9b3ff5f9b9906582b935e5cd50855330ee01
ridabayi <bayi.rida@gmail.com>
2024-06-20 04:31:20 -0700

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 
-// Get the command line arguments
 const args = process.argv.slice(2);
 
-// Check the number of arguments and print the corresponding message
 if (args.length === 0) {
   console.log('No argument');
 } else if (args.length === 1) {
