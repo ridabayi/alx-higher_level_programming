ac65ed085fbd2a7fdffccbb14ac0d369bb702ebc
ridabayi <bayi.rida@gmail.com>
2024-06-21 12:57:04 -0700

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
-#!usr/bin/node
+#!/usr/bin/node
 
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < languages.length; i++) {
-  console.log(languages[i]);
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < lines.length; i++) {
+    console.log(lines[i]);
 }
