84cedaa57fd427fa57fa449ee3c44521ed151c88
ridabayi <bayi.rida@gmail.com>
2024-06-22 01:59:03 -0700

    update

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,21 @@
 #!/usr/bin/node
 
+// Function that adds two numbers
 function add (a, b) {
   return a + b;
 }
 
-const firstArg = parseInt(process.argv[2]);
-const secondArg = parseInt(process.argv[3]);
+// Get command-line arguments
+const args = process.argv.slice(2);
 
-if (isNaN(firstArg) || isNaN(secondArg)) {
+// Parse the arguments to integers
+const num1 = parseInt(args[0]);
+const num2 = parseInt(args[1]);
+
+// Check if the arguments are valid numbers
+if (isNaN(num1) || isNaN(num2)) {
   console.log('NaN');
 } else {
-  console.log(add(firstArg, secondArg));
+  // Call the add function and print the result
+  console.log(add(num1, num2));
 }
