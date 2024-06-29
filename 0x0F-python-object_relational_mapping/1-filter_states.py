462f71b06b3fefa8924e2ee303d6aa90e22484e7
ridabayi <bayi.rida@gmail.com>
2024-06-29 04:45:35 -0700

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,40 +1,37 @@
 #!/usr/bin/python3
 """
-This script lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa.
+Script that lists all states from the database hbtn_0e_0_usa.
 """
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Get MySQL credentials and database name from command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    # Arguments: mysql_username, mysql_password, database_name
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-    # Connect to the MySQL database
-    db = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        passwd=password,
-        db=database,
-        port=3306
-    )
+    # Connect to MySQL server running on localhost at port 3306
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=mysql_username,
+                           passwd=mysql_password,
+                           db=database_name)
 
-    # Create a cursor object
-    cursor = db.cursor()
+    # Create a cursor object using cursor() method
+    cur = conn.cursor()
 
-    # Execute SQL query to select states with names starting with 'N'
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    # Execute a query
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    # Fetch all the rows from the executed query
-    rows = cursor.fetchall()
+    # Fetch all the rows using fetchall() method
+    rows = cur.fetchall()
 
     # Print the rows
     for row in rows:
         print(row)
 
-    # Close the cursor and the database connection
-    cursor.close()
-    db.close()
+    # Close the cursor and connection
+    cur.close()
+    conn.close()
