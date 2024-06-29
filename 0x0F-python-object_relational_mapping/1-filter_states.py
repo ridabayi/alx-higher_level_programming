bc9fd24a95e3fcb73bfd84ac151c9e227b49a599
ridabayi <bayi.rida@gmail.com>
2024-06-29 04:05:48 -0700

    update

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
 """
-Script that lists all states from the database hbtn_0e_0_usa.
+This script lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa.
 """
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Arguments: mysql_username, mysql_password, database_name
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    # Get MySQL credentials and database name from command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    # Connect to MySQL server running on localhost at port 3306
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=mysql_username,
-                           passwd=mysql_password,
-                           db=database_name)
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
+    )
 
-    # Create a cursor object using cursor() method
-    cur = conn.cursor()
+    # Create a cursor object
+    cursor = db.cursor()
 
-    # Execute a query
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    # Execute SQL query to select states with names starting with 'N'
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    # Fetch all the rows using fetchall() method
-    rows = cur.fetchall()
+    # Fetch all the rows from the executed query
+    rows = cursor.fetchall()
 
     # Print the rows
     for row in rows:
         print(row)
 
-    # Close the cursor and connection
-    cur.close()
-    conn.close()
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
