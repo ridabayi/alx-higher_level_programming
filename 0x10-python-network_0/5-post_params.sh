7e2e6fdc234596c96195f5c2bdcdfb11a0f7e111
ridabayi <bayi.rida@gmail.com>
2024-07-06 10:20:13 -0700

    update

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a POST request with parameters to the URL and displays the body of the response
-
 curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
