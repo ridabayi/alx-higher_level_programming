b0ead654b978cf4e0dad523011e18e3a0b275c7c
ridabayi <bayi.rida@gmail.com>
2024-07-06 10:29:44 -0700

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,18 @@
 def find_peak(list_of_integers):
-    """Finds a peak in a list of unsorted integers."""
     if not list_of_integers:
         return None
-
-    def binary_search_peak(nums, low, high):
-        while low <= high:
-            mid = (low + high) // 2
-            if (mid == 0 or nums[mid] >= nums[mid - 1]) and (mid == len(nums) - 1 or nums[mid] >= nums[mid + 1]):
-                return nums[mid]
-            elif mid > 0 and nums[mid - 1] > nums[mid]:
-                high = mid - 1
-            else:
-                low = mid + 1
-        return None
-
-    return binary_search_peak(list_of_integers, 0, len(list_of_integers) - 1)
+    
+    left, right = 0, len(list_of_integers) - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
+           (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
+            return list_of_integers[mid]
+        elif mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return None
