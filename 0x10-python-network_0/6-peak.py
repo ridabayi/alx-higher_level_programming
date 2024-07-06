e16e203908d3267790cbb3655b20ed3b7566403d
ridabayi <bayi.rida@gmail.com>
2024-07-06 10:24:37 -0700

    update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,12 +4,14 @@ def find_peak(list_of_integers):
         return None
 
     def binary_search_peak(nums, low, high):
-        mid = (low + high) // 2
-        if (mid == 0 or nums[mid] >= nums[mid - 1]) and (mid == len(nums) - 1 or nums[mid] >= nums[mid + 1]):
-            return nums[mid]
-        elif mid > 0 and nums[mid - 1] > nums[mid]:
-            return binary_search_peak(nums, low, mid - 1)
-        else:
-            return binary_search_peak(nums, mid + 1, high)
+        while low <= high:
+            mid = (low + high) // 2
+            if (mid == 0 or nums[mid] >= nums[mid - 1]) and (mid == len(nums) - 1 or nums[mid] >= nums[mid + 1]):
+                return nums[mid]
+            elif mid > 0 and nums[mid - 1] > nums[mid]:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return None
 
     return binary_search_peak(list_of_integers, 0, len(list_of_integers) - 1)
