a1fc1f9adc694b84121b1ba2f50a66d3b5f84e72
ridabayi <bayi.rida@gmail.com>
2024-07-09 03:57:08 -0700

    up

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """Finds a peak in a list of unsorted integers"""
 
-
-
-def find_peak(arr):
+def find_peak(list_of_integers):
     """Find a peak element in the list of unsorted integers."""
-    if not arr:
+    if not list_of_integers:
         return None
 
-    return peak_helper(arr, 0, len(arr) - 1)
+    return find_peak_helper(list_of_integers, 0, len(list_of_integers) - 1)
 
-def peak_helper(arr, low, high):
+def find_peak_helper(arr, low, high):
     """Helper function to find the peak element using binary search."""
     mid = (low + high) // 2
 
@@ -19,7 +17,7 @@ def peak_helper(arr, low, high):
         return arr[mid]
     # If the left neighbor is greater, then the peak must be on the left side
     elif mid > 0 and arr[mid - 1] > arr[mid]:
-        return peak_helper(arr, low, mid - 1)
+        return find_peak_helper(arr, low, mid - 1)
     # If the right neighbor is greater, then the peak must be on the right side
     else:
-        return peak_helper(arr, mid + 1, high)
+        return find_peak_helper(arr, mid + 1, high)
