8733c426b3f7f42c4df94c54ff43d38af8a1c7fe
ridabayi <bayi.rida@gmail.com>
2024-07-09 03:59:31 -0700

    up

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """Finds a peak in a list of unsorted integers"""
 
+
 def find_peak(list_of_integers):
-    """Find a peak element in the list of unsorted integers."""
-    if not list_of_integers:
+    """Finds a peak in list_of_integers"""
+
+    if list_of_integers is None or list_of_integers == []:
         return None
-
-    return find_peak_helper(list_of_integers, 0, len(list_of_integers) - 1)
-
-def find_peak_helper(arr, low, high):
-    """Helper function to find the peak element using binary search."""
-    mid = (low + high) // 2
-
-    # If mid is a peak
-    if (mid == 0 or arr[mid - 1] <= arr[mid]) and (mid == len(arr) - 1 or arr[mid + 1] <= arr[mid]):
-        return arr[mid]
-    # If the left neighbor is greater, then the peak must be on the left side
-    elif mid > 0 and arr[mid - 1] > arr[mid]:
-        return find_peak_helper(arr, low, mid - 1)
-    # If the right neighbor is greater, then the peak must be on the right side
-    else:
-        return find_peak_helper(arr, mid + 1, high)
+    lo = 0
+    hi = len(list_of_integers)
+    mid = ((hi - lo) // 2) + lo
+    mid = int(mid)
+    if hi == 1:
+        return list_of_integers[0]
+    if hi == 2:
+        return max(list_of_integers)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid:])
+    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
