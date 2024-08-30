b9cc8512eb5312158b70262b60646cd437dcbcb7
ridabayi <bayi.rida@gmail.com>
2024-08-30 09:15:58 -0700

    add

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """
-Defines a state model that contain the class definition
- of a State and an instance Base = declarative_base()
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
 """
-from lib2to3.pytree import Base
-from sre_parse import State
-from unicodedata import name
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -13,17 +11,14 @@ Base = declarative_base()
 
 
 class State(Base):
-    """
-    inherits from Base Tips
-    links to the MySQL table states
-    class attribute id that represents a column
-     of an auto-generated, unique integer, can't
-      be null and is a primary key
-    class attribute name that represents a column
-     of a string with maximum 128 characters and
-      can't be null
+    """State class
 
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
     """
-    __tablename__ = "states"
+    __tablename__ = 'states'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
