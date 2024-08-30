c146bf65978fce3be74a105ef16b92aae21a04ec
ridabayi <bayi.rida@gmail.com>
2024-08-30 09:00:02 -0700

    add files up

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
+"""
+lists all State objects that contain
+the letter a from the database hbtn_0e_6_usa
+"""
+
+import imp
+import sys
+from venv import create
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-import sys
+from model_state import State
 
 if __name__ == "__main__":
-    # Get MySQL credentials from command-line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
 
-    # Create the engine to connect to MySQL
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}', pool_pre_ping=True)
-
-    # Bind the engine to the metadata of the Base class so that the declaratives can be accessed through a DBSession instance
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Query the State table for states containing the letter 'a' and order by id
-    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
-
-    # Print the results
-    for state in states:
-        print(f"{state.id}: {state.name}")
-
-    # Close the session
-    session.close()
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
