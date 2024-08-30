d1471aa48c2268a3ff3a435ec90809af3dc945e1
ridabayi <bayi.rida@gmail.com>
2024-08-30 03:23:03 -0700

    add files up

--- 0x13-javascript_objects_scopes_closures/7-occurences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurences.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  list.map(
-    (x) => {
-      if (x === searchElement) {
-        count += 1;
-        return 1;
-      } else {
-        return 0;
-      }
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count += 1;
     }
-  );
+  }
   return count;
 };
