baf0797e7a0a045405dd9607fa7c0d5a6af25d2d
ridabayi <bayi.rida@gmail.com>
2024-09-06 01:37:59 -0700

    add

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-import requests
+"""Fetches https://alx-intranet.hbtn.io/status."""
+import urllib.request
 
-def fetch_github_user(username):
-    url = f"https://api.github.com/users/{username}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        user_data = response.json()
-        print("User Data:")
-        print(f"\t- Login: {user_data.get('login')}")
-        print(f"\t- ID: {user_data.get('id')}")
-        print(f"\t- Name: {user_data.get('name')}")
-        print(f"\t- Public Repos: {user_data.get('public_repos')}")
-    else:
-        print(f"Failed to retrieve data: {response.status_code}")
 
 if __name__ == "__main__":
-    username = "ridabayi"  
-    fetch_github_user(username)
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
