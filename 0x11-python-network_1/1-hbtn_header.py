0ae3625f71a890288cf31f95c0f7917ddcd186f8
ridabayi <bayi.rida@gmail.com>
2024-09-06 01:43:27 -0700

    add

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-"""Fetches and prints the value of the X-Request-Id header from a URL.
+"""Fetches and prints the value of the X-Request-Id header from a URL
 Usage: ./1-hbtn_header.py <URL>
 """
 import sys
 import urllib.request
 
-def fetch_request_id(url):
-    """Fetches the X-Request-Id header from the provided URL."""
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        headers = dict(response.headers)
-        return headers.get("X-Request-Id")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./1-hbtn_header.py <URL>")
-        sys.exit(1)
-    
     url = sys.argv[1]
-    print(fetch_request_id(url))
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
