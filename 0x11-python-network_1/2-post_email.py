907ba0bfec8e86b503ed21a2968d66d86ce60bef
ridabayi <bayi.rida@gmail.com>
2024-09-09 02:48:32 -0700

    update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Sends a POST request to a given URL with an email parameter and displays the response body.
+Script that takes in a URL and an email, sends a POST request to the passed URL with the
+email as a parameter, and displays the body of the response.
 """
 if __name__ == "__main__":
     import urllib.parse as parse
